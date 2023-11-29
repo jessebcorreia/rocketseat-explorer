2a9a3abe943dbb36e0bd8ae004b857afcdb6942e
= <jesse.bcorreia@gmail.com>
2023-11-29 20:51:37 -0300

    update

--- exercises_old/ex017 - ES6/main.js
+++
@@ -1,3 +0,0 @@
-import * as calculator from './utils.js'
-
-console.log(calculator.multiply(49, 4))
--- exercises_old/ex017 - ES6/utils.js
+++
@@ -1,7 +0,0 @@
-const sum = (a, b) => a + b
-const multiply = (a, b) => a * b
-const subtract = (a, b) => a - b
-const divide = (a, b) => a / b
-
-// ou 
-export { sum, multiply, subtract, divide }
