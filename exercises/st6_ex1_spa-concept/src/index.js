d10a1c2317e5a58fb23be9f8611c31b8132b1bc6
= <jesse.bcorreia@gmail.com>
2023-12-02 11:06:17 -0300

    update

--- exercises/st6_ex1_spa-concept/src/index.js
+++ exercises/st6_ex1_spa-concept/src/index.js
@@ -1,13 +1,24 @@
-import { Router } from './router.js'
+import { Router } from "./router.js"
 
 const router = new Router()
+// estou criando um objeto, tomando como base a classe Router
 
-router.add("/"          ,"/pages/home.html")
-router.add("/about"     ,"/pages/about.html")
-router.add("/contact"   ,"/pages/contact.html")
-router.add(404          ,"/pages/404.html")
+// abaixo, vou executar a função definida dentro da classe e instanciada no objeto
+// vou adicionar dentro do objeto routes o nome da propriedade, e o valor
+router.add('/'       , '/pages/home.html')
+router.add('/about'  , '/pages/about.html')
+router.add('/contact', '/pages/contact.html')
+router.add(404       , '/pages/404.html')
 
+// como o objeto foi instanciado de uma classe...
+// vou precisar chamar o método/função que está dentro da classe .
 router.handle()
 
-window.onpopstate = () => router.handle()
-window.route      = () => router.route()
+window.addEventListener('popstate', router.handle)
+//window.onpopstate = () => router.handle()
+
+// eventos de click da navegação estão sendo atribuidos direto no index.html
+
+window.route = () => router.route()
+// atribuo na window a função route
+
--- exercises/st6_ex1_spa-concept/src/router.js
+++ exercises/st6_ex1_spa-concept/src/router.js
@@ -1,29 +1,51 @@
 export class Router {
 
   routes = {}
+  // estou criando um objeto vazio, que receberá os valores pela função
 
-  add(routeName, page) {
-    this.routes[routeName] = page
+  add(routeName, pageLink) {
+    this.routes[routeName] = pageLink
+    // o objeto routes está sendo criado dentro da classe
+    // na mesma classe, há esta função que adiciona novas propriedades ao objeto
+    // resumindo: o objeto routes [propriedade] recebe o valor = valor
   }
 
   route(event) {
     event = event || window.event
     event.preventDefault()
-
-    window.history.pushState({}, "", event.target.href)
-    this.handle()
-  }
-
-  handle(){
-    const { pathname } = window.location
-    const route = this.routes[pathname] || this.routes[404]
-    
-    fetch(route)
-      .then( data => data.text() )
-      .then( html => {
-        document.querySelector('#app').innerHTML = html
-      }
-    )
-  }
   
+    window.history.pushState({}, "", event.target.href)
+    //quem disparou o evento (target) foi uma tag 'a', que possuir href
+    //com essa função, estou pegando o href e colocando no histórico
+  
+    this.handle()
+    //this.handle()
+    // é necessário utilizar o "this."
+    // será a referência à instância/objeto que será criado
+  }
+
+  handle() {
+    const { pathname } = window.location
+      //const pathname = window.location.pathname
+      //const href     = window.location.href
+    const route = this.routes[pathname] || this.routes[404]
+      //pathname está retornando uma string "/nomeDaRota"
+      //a string está servindo de parâmetro para acessar a propriedade
+      //se a rota não estiver mapeada, mostrar a rota 404
+  
+    fetch(route)
+      .then(
+        data => data.text()
+        // function (data) { return data.text()
+        // retorna o valor para o próximo .then }
+      )
+      .then(
+        // html é o nome do parâmetro que está recebendo o retorno acima
+        //function (html) { return document.querySelector('#app').innerHTML = html }
+        html => { document.querySelector('#app').innerHTML = html }
+      )
+  }
+
 }
+
+
