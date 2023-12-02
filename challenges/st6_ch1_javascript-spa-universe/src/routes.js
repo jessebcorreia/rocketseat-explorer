3bc02621430944c9895ceb38ef6e860126e91680
= <jesse.bcorreia@gmail.com>
2023-12-02 13:53:45 -0300

    update

--- challenges/st6_ch1_javascript-spa-universe/src/routes.js
+++ challenges/st6_ch1_javascript-spa-universe/src/routes.js
@@ -25,6 +25,43 @@ export class Router {
       .then( resultDataHTML => {
         document.querySelector("#app").innerHTML = resultDataHTML
       })
+
+    this.checkPage()
+  }
+
+  checkPage(){
+
+    const linkHome = document.querySelector('#link-home')
+    const linkUniverse = document.querySelector('#link-universe')
+    const linkExploration = document.querySelector('#link-exploration')
+    const bodyHtml = document.querySelector('body')
+  
+    const {pathname} = window.location
+  
+    linkHome.classList.remove('active')
+    linkUniverse.classList.remove('active')
+    linkExploration.classList.remove('active')
+  
+    switch(pathname) {
+      case "/":
+        linkHome.classList.add('active')
+        bodyHtml.style.backgroundImage = "url('/assets/mountains-universe-1.png')"
+        break
+  
+      case "/universe":
+        linkUniverse.classList.add('active')
+        bodyHtml.style.backgroundImage = "url('/assets/mountains-universe-2.png')"
+        break
+  
+      case "/exploration":
+        linkExploration.classList.add('active')
+        bodyHtml.style.backgroundImage = "url('/assets/mountains-universe-3.png')"
+        break
+  
+      default:
+        break
+    }
+  
   }
 
 }
