6c03e63442fdd543eefdd77fdf3a56ad98cba776
= <jesse.bcorreia@gmail.com>
2023-12-05 00:17:03 -0300

    update

--- exercises/st6_ex2_github-favorites/js/main.js
+++ exercises/st6_ex2_github-favorites/js/main.js
@@ -1,14 +1,3 @@
-import { Router } from "./router.js";
 import { FavoritesView } from "./favorites.js"
-
-const router = new Router()
-
-router.add("/", "/pages/home.html")
-router.add("/git-fav", "/pages/git-fav.html")
-router.add(404, "/pages/404.html")
-
-router.handle()
-window.onpopstate = () => router.handle()
-window.route = () => router.route()
 
 new FavoritesView("#app")
